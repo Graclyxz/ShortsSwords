a71e8a1a0950d021ae5ad68be42aa8e7bbc38fb3
Xavier Villavicencio <jxavier2525@gmail.com>
2024-12-10 04:58:06 -0500

    1.21.4 version init

---
+++ common/src/main/java/com/graclyxz/shortswords/Constants.java
@@ -0,0 +1,11 @@
+package com.graclyxz.shortswords;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "shortswords";
+	public static final String MOD_NAME = "Short Swords";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/com/graclyxz/shortswords/init/ModTab.java
@@ -0,0 +1,29 @@
+package com.graclyxz.shortswords.init;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+
+import static com.graclyxz.shortswords.Constants.MOD_ID;
+
+public class ModTab {
+
+    public static final CreativeModeTab TAB_SHORTS_SWORDS = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.SHORT_SWORDS.get(5)))
+            .displayItems((features, event) -> {
+                for (Item item : ModItems.ITEMS)
+                    event.accept(item);
+            })
+            .title(Component.translatable("item_group.shortswords.tab_shorts_swords"))
+            .build();
+
+    public static void init() {
+        Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, ResourceLocation.fromNamespaceAndPath(MOD_ID, "tab_shortswords"), TAB_SHORTS_SWORDS);
+    }
+}
